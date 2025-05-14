8b55bd624946fd8a2d8d2f8afa9ad970efa53ae8
LamLuong <lamluongbka@gmail.com>
2025-05-14 23:44:27 +0700

    [DEV]initial commit

---
+++ src/main/java/com/bkazx/aicam/AicamApplication.java
@@ -0,0 +1,13 @@
+package com.bkazx.aicam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AicamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicamApplication.class, args);
+	}
+
+}
