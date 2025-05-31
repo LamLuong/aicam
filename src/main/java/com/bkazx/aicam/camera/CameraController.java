9f5f905e67ffba7f05da036fc7eb895706865a28
lamluong <lamluongbka@gmail.com>
2025-06-01 00:24:08 +0700

    [DEV] Add swagger for authenticating

--- src/main/java/com/bkazx/aicam/camera/CameraController.java
+++ src/main/java/com/bkazx/aicam/camera/CameraController.java
@@ -1,12 +1,19 @@
 package com.bkazx.aicam.camera;
 
-import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 
 @RestController
 @RequestMapping(value = "/aicam/v1")
@@ -19,6 +26,7 @@ public class CameraController {
   }
 
   @GetMapping(path = "/cameras")
+  @Operation(summary = "Get all cameras", security = @SecurityRequirement(name = "bearerAuth"))
   public List<Camera> getListCameras() {
     return cameraRepository.findAll();
 
--- src/main/java/com/bkazx/aicam/config/SwaggerConfiguration.java
+++ src/main/java/com/bkazx/aicam/config/SwaggerConfiguration.java
@@ -1,14 +1,15 @@
 package com.bkazx.aicam.config;
 
+import org.springframework.stereotype.Component;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springframework.stereotype.Component;
 
 @Component
 @OpenAPIDefinition(info = @Info(title = "AI Camera APIs", version = "v1"))
-// @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class SwaggerConfiguration {
 
 }
